7295b31ed0c4521b7ab38ffa176523afddf96592
MaouLim <502329767@qq.com>
2017-07-06 10:15:16 +0800

    添加了TCPHelper

---
+++ app/src/main/java/bupt/wifidirectchat/service/tcp/TCPHelper.java
@@ -0,0 +1,25 @@
+package bupt.wifidirectchat.service.tcp;
+
+/*
+ * Created by Maou on 2017/7/6.
+ */
+
+import bupt.wifidirectchat.util.SimpleThreadPool;
+
+/*
+ * the TCPHelper is to help start the tcp components
+ */
+public class TCPHelper {
+
+	public static void startListener(Listener listener) {
+		SimpleThreadPool.execute(listener);
+	}
+
+	public static void startConnector(Connector connector) {
+		SimpleThreadPool.execute(connector);
+	}
+
+	public static void startCommunicator(Communicator communicator) {
+		SimpleThreadPool.execute(communicator);
+	}
+}
